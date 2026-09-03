d794193175878c26d0b6cfeb167993fe463742d6
A Mad Dev <muhammedaadhilv69@gmail.com>
2026-09-03 07:04:06 +0300

    Fix: TS Error + proper implementation in Project.tsx page

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,5 +1,13 @@
 import {type Project} from "@/types/globals";
+import {useParams} from "react-router-dom";
+import { projects } from "@/data/projects";
 
-export default function Project({project}: {project: Project}) {
-    return <div className="p-4">{project.name}</div>
+export default function Project() {
+    const params = useParams<string>();
+    const project: Project = projects.find((p) => p.slug === params.slug) as Project;
+
+    if (!project) {
+      return <div>Project not found</div>;
+    }
+    return <div className="p-4">{project.title}</div>
 }
