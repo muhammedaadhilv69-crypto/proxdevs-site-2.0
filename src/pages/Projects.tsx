74c28a281d41796c3a5e70c650ee48bc89a52ae0
A Mad Dev <muhammedaadhilv69@gmail.com>
2026-09-03 07:13:47 +0300

    fix: Vercel routing bug via vercel.json

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -6,9 +6,9 @@ export default function Projects() {
   return (
     <div className="p-8">
       <div>
-        <h1 className="text-2xl font-bold">Projects</h1>
+        <h1 className="text-5xl font-bold">Projects</h1>
       </div>
-      <Separator className="my-4" />
+      <Separator className="mt-4 mb-8" />
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
         {projects.map((project) => (
           <ProjectCard key={project.slug} project={project} />
